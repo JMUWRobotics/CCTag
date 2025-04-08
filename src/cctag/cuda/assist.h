726ccc0e1d621907a78d0bed9fc9722125b461f6
Christoph Liebender <christoph.liebender@uni-wuerzburg.de>
2025-04-08 10:59:04 +0200

    update CMakeLists.txt

--- src/cctag/cuda/assist.h
+++ src/cctag/cuda/assist.h
@@ -52,7 +52,6 @@ inline int d_sign( T value )
     return ( ( value < 0 ) ? -1 : 1 );
 }
 
-#ifdef CCTAG_HAVE_SHFL_DOWN_SYNC
 template<typename T> __device__ inline T shuffle     ( T variable, int src   ) { return __shfl_sync     ( 0xffffffff, variable, src   ); }
 template<typename T> __device__ inline T shuffle_up  ( T variable, int delta ) { return __shfl_up_sync  ( 0xffffffff, variable, delta ); }
 template<typename T> __device__ inline T shuffle_down( T variable, int delta ) { return __shfl_down_sync( 0xffffffff, variable, delta ); }
@@ -65,20 +64,6 @@ template<typename T> __device__ inline T shuffle     ( T variable, int src  , in
 template<typename T> __device__ inline T shuffle_up  ( T variable, int delta, int ws ) { return __shfl_up_sync  ( 0xffffffff, variable, delta, ws ); }
 template<typename T> __device__ inline T shuffle_down( T variable, int delta, int ws ) { return __shfl_down_sync( 0xffffffff, variable, delta, ws ); }
 template<typename T> __device__ inline T shuffle_xor ( T variable, int delta, int ws ) { return __shfl_xor_sync ( 0xffffffff, variable, delta, ws ); }
-#else
-template<typename T> __device__ inline T shuffle     ( T variable, int src   ) { return __shfl     ( variable, src   ); }
-template<typename T> __device__ inline T shuffle_up  ( T variable, int delta ) { return __shfl_up  ( variable, delta ); }
-template<typename T> __device__ inline T shuffle_down( T variable, int delta ) { return __shfl_down( variable, delta ); }
-template<typename T> __device__ inline T shuffle_xor ( T variable, int delta ) { return __shfl_xor ( variable, delta ); }
-__device__ inline unsigned int ballot( unsigned int pred ) { return __ballot   ( pred ); }
-__device__ inline int any            ( unsigned int pred ) { return __any      ( pred ); }
-__device__ inline int all            ( unsigned int pred ) { return __all      ( pred ); }
-
-template<typename T> __device__ inline T shuffle     ( T variable, int src  , int ws ) { return __shfl     ( variable, src  , ws ); }
-template<typename T> __device__ inline T shuffle_up  ( T variable, int delta, int ws ) { return __shfl_up  ( variable, delta, ws ); }
-template<typename T> __device__ inline T shuffle_down( T variable, int delta, int ws ) { return __shfl_down( variable, delta, ws ); }
-template<typename T> __device__ inline T shuffle_xor ( T variable, int delta, int ws ) { return __shfl_xor ( variable, delta, ws ); }
-#endif
 
 __device__
 inline
